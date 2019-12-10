34bdfc7365e2a3f41708b3bd2fc80c5dacba0f4d
Edgardo <Edgardo@DESKTOP-HRVT699>
2019-12-10 14:44:34 -0300

    Se agrega un constructor para recibir IMessageManager

--- src/ar/com/tbf/api/essential/exception/NotFound404Exception.java
+++ src/ar/com/tbf/api/essential/exception/NotFound404Exception.java
@@ -3,12 +3,16 @@ package ar.com.tbf.api.essential.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import ar.com.tbf.common.application.helper.IMessageManager;
+
 public class NotFound404Exception extends ResponseStatusException{
 
 	private static final long serialVersionUID = -410846358602528931L;
 	
+	private IMessageManager messageManager;
+	
 	/**
-	 * EnvÃ­a un 404 con la descripciÃ³n del o los errores
+	 * Envía un 404 con la descripción del o los errores
 	 * @param reason
 	 */
 	public NotFound404Exception( String reason ) {
@@ -17,12 +21,26 @@ public class NotFound404Exception extends ResponseStatusException{
 	}
 	
 	/**
-	 * @See ValidationException( String reason )
 	 * @param status
 	 * @param reason
 	 */
 	public NotFound404Exception(HttpStatus status, String reason) {
 		super(status, reason);
-		
 	}
+	
+	public NotFound404Exception( IMessageManager reason ) {
+		
+		this( HttpStatus.NOT_FOUND, "" );
+		
+		this.setMessageManager(reason);
+	}
+
+	public IMessageManager getMessageManager() {
+		return messageManager;
+	}
+
+	public void setMessageManager(IMessageManager messageManager) {
+		this.messageManager = messageManager;
+	}
+
 }
